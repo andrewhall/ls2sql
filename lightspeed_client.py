45fbe8f92fb5fc99722cd18449aa032ba4f3495d
Default <ahall@sunsetnovelties.com>
2017-03-28 16:49:40 -0400

    Consolidated summary calls into single function.

--- lightspeed_client.py
+++ lightspeed_client.py
@@ -9,7 +9,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def process_store(current_store):
-    logging.info('%s: Store initiating.' % current_store['store'])
+    logging.info('%s | Store initiating.' % current_store['store'])
 
     # Create persistent API session
     session = requests.Session()
@@ -19,94 +19,46 @@ def process_store(current_store):
                             'X-PAPPID': 'ee18ea41-2705-4189-b628-095d995d0d33',
                             'Content-Type': 'text/xml'})
 
-    imbed_invoices(current_store, session)
-    # capture_customers(current_store, session)
-    # unite_users(current_store, session)
-    # salvage_suppliers(current_store, session)
-    # procure_purchase_orders(current_store, session)
-    # produce_products(current_store, session)
+    # Do work, bitch
+    process_summary_records(current_store, session)
+    process_detailed_records(current_store, session)
 
-    # close API session gracefully
+    # Close API session gracefully
     session.close()
 
-    logging.info('%s: Store completed.' % current_store['store'])
+    logging.info('%s | Store completed.' % current_store['store'])
 
 
-def imbed_invoices(current_store, session):
-
-    logging.info('%s: Invoice data initiating.' % current_store['store'])
-
-    invoice_list = []
-    invoice_filter = {'filter': 'id > \"%s\" AND id <= \"%s\"' % (current_store['last_invoice'],
-                                                                  current_store['last_invoice'] + 250)}
-    response = session.get(current_store['address'] + 'invoices/', params=invoice_filter)
-
-    root = ElementTree.fromstring(response.text)
-
-    for child in root:
-        invoice_uri = child.get('uri')
-        invoice_list.append(invoice_uri)
-
-    insert_values = []
-
-    for invoice in invoice_list:
-        response = session.get(invoice)
-
-        root = ElementTree.fromstring(response.text)
-
-        temp_values = (current_store['storecode'], root.tag, ElementTree.tostring(root))
-
-        insert_values.append(temp_values)
-
-    db_client.insert_xml(insert_values)
-
-    logging.info('%s: Invoice data completed.' % current_store['store'])
-
-
-def capture_customers(current_store, session):
-
-    logging.info('%s: Customer data initiating.' % current_store['store'])
-
-    response = session.get(current_store['address'] + 'customers/')
-
-    root = ElementTree.fromstring(response.text)
-
-    insert_values = [(current_store['storecode'], root.tag, ElementTree.tostring(root))]
-
-    db_client.insert_xml(insert_values)
-
-    logging.info('%s: Customer data completed.' % current_store['store'])
-
-
-def unite_users(current_store, session):
-
-    logging.info('%s: User data initiating.' % current_store['store'])
-
-    response = session.get(current_store['address'] + 'users/')
-
-    root = ElementTree.fromstring(response.text)
-
-    insert_values = [(current_store['storecode'], root.tag, ElementTree.tostring(root))]
-
-    db_client.insert_xml(insert_values)
-
-    logging.info('%s: User data completed.' % current_store['store'])
-
-
-def salvage_suppliers(current_store, session):
-
-    logging.info('%s: Supplier data initiating.' % current_store['store'])
-
-    response = session.get(current_store['address'] + 'suppliers/')
-
-    root = ElementTree.fromstring(response.text)
-
-    insert_values = [(current_store['storecode'], root.tag, ElementTree.tostring(root))]
-
-    db_client.insert_xml(insert_values)
-
-    logging.info('%s: Supplier data completed.' % current_store['store'])
-
+# def imbed_invoices(current_store, session):
+#
+#     logging.info('%s: Invoice data initiating.' % current_store['store'])
+#
+#     invoice_list = []
+#     invoice_filter = {'filter': 'id > \"%s\" AND id <= \"%s\"' % (current_store['last_invoice'],
+#                                                                   current_store['last_invoice'] + 250)}
+#     response = session.get(current_store['address'] + 'invoices/', params=invoice_filter)
+#
+#     root = ElementTree.fromstring(response.text)
+#
+#     for child in root:
+#         invoice_uri = child.get('uri')
+#         invoice_list.append(invoice_uri)
+#
+#     insert_values = []
+#
+#     for invoice in invoice_list:
+#         response = session.get(invoice)
+#
+#         root = ElementTree.fromstring(response.text)
+#
+#         temp_values = (current_store['storecode'], root.tag, ElementTree.tostring(root))
+#
+#         insert_values.append(temp_values)
+#
+#     db_client.insert_xml(insert_values)
+#
+#     logging.info('%s: Invoice data completed.' % current_store['store'])
+#
 
 # def procure_purchase_orders(current_store):
 #
@@ -145,20 +97,29 @@ def salvage_suppliers(current_store, session):
 #     logging.info('%s: PO data completed.' % current_store['store'])
 #
 #
-# def produce_products(current_store):
-#
-#     logging.info('%s: Product data initiating.' % current_store['store'])
-#
-#     invoice_url = current_store['address'] + 'products/'
-#
-#     response = requests.get(invoice_url,
-#                             auth=(current_store['username'], current_store['password']),
-#                             headers=headers,
-#                             verify=False)
-#     root = ElementTree.fromstring(response.text)
-#
-#     insert_values = [(current_store['storecode'], root.tag, ElementTree.tostring(root))]
-#
-#     db_client.insert_xml(insert_values)
-#
-#     logging.info('%s: Product data completed.' % current_store['store'])
+
+def process_summary_records(current_store, session):
+
+    # types of records processed in this method
+    document_types = ['customers', 'users', 'suppliers', 'products']
+
+    # Loop through
+    for document_type in document_types:
+
+        logging.info('%s | Initiating: %s data.' % (current_store['storecode'], document_type))
+
+        response = session.get(current_store['address'] + document_type + '/')
+
+        root = ElementTree.fromstring(response.text)
+
+        insert_values = [(current_store['storecode'], root.tag, ElementTree.tostring(root))]
+
+        db_client.insert_xml(insert_values)
+
+        logging.info('%s | Completed: %s data.' % (current_store['storecode'], document_type))
+
+
+def process_detailed_records(current_store, session):
+
+    # types of records processed in this method
+    document_types = ['invoices', 'purchase_orders']
